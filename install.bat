e64fcb3dcc9d20c7f5f070ffe6bff31b43fdd00c
Amped <52733865+AmpedWasTaken@users.noreply.github.com>
2022-08-11 02:28:47 +0200

    made a check if node is installed

--- install.bat
+++ install.bat
@@ -1,2 +1,9 @@
 @echo off
-npm i fivem-script-converter
+node -v 2> Nul
+if "%errorlevel%" == "9009" (
+    echo node could not be found
+    echo make sure node is installed
+    pause
+) else (
+    npm i fivem-script-converter
+)
